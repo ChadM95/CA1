6b60babe73a1c83cdf93b55ff5bf5fe1281fa9c4
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-08 21:34:40 +0000

    commit

---
+++ CA1/CA1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CA1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime MyDate = new DateTime();
+
+        /*   Console.WriteLine("Enter your Date of Birth...");
+           Console.Write("Year > ");*/
+
+        MyDate.AddYears(2023);
+
+        Console.WriteLine(MyDate);
+        Console.ReadLine();
+    }
+}
+
