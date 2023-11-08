c2eea962a8015b3c79f84a9d2d6caa522bd602a4
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-08 22:47:24 +0000

    finish q3

--- CA1/Question3/Program.cs
+++ CA1/Question3/Program.cs
@@ -4,7 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
+        double oldGrade, newGrade;
+
+        Console.Write("Enter grade > ");
+        oldGrade = double.Parse(Console.ReadLine());
+
+        newGrade = Add15Percent(oldGrade);
+
+        Console.WriteLine("Your new grade is > {0:F0}",newGrade);
+
         Console.ReadLine();
+    }
+
+    static double Add15Percent(double userGrade)
+    {
+        if (userGrade > 0 && userGrade <= 100)
+        { return userGrade + (userGrade * 0.15); }
+        else { return 0;};
     }
 }
 
