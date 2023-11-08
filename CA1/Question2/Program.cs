4e00382a5e1333c74d5acbba9b395cdcd726001d
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-08 22:18:15 +0000

    add variables and input to q2

--- CA1/Question2/Program.cs
+++ CA1/Question2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
+        //variables
+        int basicSeatCost, bags10kg, bags20kg, meals, reservedSeats;
+
+        //input
+        Console.Write("Please enter seat cost > ");
+        basicSeatCost = int.Parse(Console.ReadLine());
+
+        Console.Write("Please enter number of 10kg bags > ");
+        bags10kg = int.Parse(Console.ReadLine());
+
+        Console.Write("Please enter number of 20kg bags > ");
+        bags20kg = int.Parse(Console.ReadLine());
+
+        Console.Write("Please enter number of on-board meals > ");
+        meals = int.Parse(Console.ReadLine());
+
+        Console.Write("Please enter number of reserved seats > ");
+        reservedSeats = int.Parse(Console.ReadLine());
+
+        //calculations
+
+
+
+        //output
+
         Console.ReadLine();
     }
 }
