c09ac3a7f5672e804bd0a12d2f2a7e9f811e328f
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-08 21:44:31 +0000

    Q1 first version

--- CA1/CA1/Program.cs
+++ CA1/CA1/Program.cs
@@ -4,14 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        DateTime MyDate = new DateTime();
+        string name;
+        int day, month, year;
 
-        /*   Console.WriteLine("Enter your Date of Birth...");
-           Console.Write("Year > ");*/
+        Console.Write("Enter your name > ");
+        name = Console.ReadLine();
 
-        MyDate.AddYears(2023);
+        Console.WriteLine("Enter your date of birth in dd mm yyyy format:");
+        Console.Write("Day > ");
+        day = int.Parse(Console.ReadLine());
 
-        Console.WriteLine(MyDate);
+        Console.Write("month > ");
+        month = int.Parse(Console.ReadLine());
+
+        Console.Write("year > ");
+        year = int.Parse(Console.ReadLine());
+
+
+        DateTime DateOfBirth = new DateTime(year, month, day);
+
+        Console.WriteLine(DateOfBirth);
+
         Console.ReadLine();
     }
 }
