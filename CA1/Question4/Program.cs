375a61e1dbbfaf6989e2b4e76ff34f500977f5c0
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-09 21:42:21 +0000

    add input methods

--- CA1/Question4/Program.cs
+++ CA1/Question4/Program.cs
@@ -4,7 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
+        string name;
+        int monthlySales, salesTarget;
+
+        name = AskName();
+
+        monthlySales = AskSales();
+
+        salesTarget = AskSalesTarget();
+
+
         Console.ReadLine();
+    }
+
+    static string AskName()
+    {
+        Console.Write("Enter name > ");
+        return Console.ReadLine();
+    }
+
+    static int AskSales()
+    {
+        Console.Write("Enter monthly sales > ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int AskSalesTarget()
+    {
+        Console.Write("Enter monthly sales target > ");
+        return int.Parse(Console.ReadLine());
     }
 }
 
