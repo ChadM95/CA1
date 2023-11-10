4f10cca6488d7d8148bb7ff6e1c114b4e0c11826
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-10 21:25:48 +0000

    create class and constructor

---
+++ CA1/Question5/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Question5
+{
+	public class Product
+	{
+		public string ProductName { get; set; }
+        public int ProductID { get; set; }
+        public int Price { get; set; }
+        public string Category { get; set; }
+
+        public Product(string productName, int productID, int price, string category)
+		{
+			productName = ProductName;
+			productID = ProductID;
+			price = Price;
+			category = Category;
+		}
+	}
+}
+
