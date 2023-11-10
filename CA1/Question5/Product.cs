617a320684e4a85a71c2c7f4724e6406c4c1f74a
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-10 21:29:33 +0000

    add method

--- CA1/Question5/Product.cs
+++ CA1/Question5/Product.cs
@@ -3,11 +3,13 @@ namespace Question5
 {
 	public class Product
 	{
+		//properties
 		public string ProductName { get; set; }
         public int ProductID { get; set; }
         public int Price { get; set; }
         public string Category { get; set; }
 
+		//constructor
         public Product(string productName, int productID, int price, string category)
 		{
 			productName = ProductName;
@@ -15,6 +17,15 @@ namespace Question5
 			price = Price;
 			category = Category;
 		}
+
+		//method
+		public void PrintProductDetails()
+		{
+			Console.WriteLine($"Product name     = {ProductName}");
+            Console.WriteLine($"Product ID       = {ProductID}");
+            Console.WriteLine($"Product Price    = {Price}");
+            Console.WriteLine($"Product Category = {Category}");
+        }
 	}
 }
 
