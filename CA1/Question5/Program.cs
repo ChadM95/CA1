96547bd169b63f4c50cfc7c0ceff6d4880ad14dd
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2023-11-10 21:33:12 +0000

    instantiate 2 objects

--- CA1/Question5/Program.cs
+++ CA1/Question5/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        //instantiate product objects
+
+        Product P1 = new Product("Rip Curl Dawn Patrol", 1, 320, "Wetsuit");
+        Product P2 = new Product("Pyzel" , 2, 600, "Surfboard") ;
+
         Console.ReadLine();
     }
 }
